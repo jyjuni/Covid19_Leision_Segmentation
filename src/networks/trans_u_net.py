f9ddd4f03ac838cb1805137552c5b189e80663d3
jyjuni <yj2682@columbia.edu>
2022-04-27 19:27:42 -0400

    initial

---
+++ src/networks/trans_u_net.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+    TransUNet model    
+"""
+
+__author__ = "Yijia Jin"
+__copyright__ = "Copyright 2022, Yijia Jin"
+__version__ = "1.0.0"
+__email__ = "yj2682@columbia.edu"
+
+
+import torch
+
+class TransUNet(torch.nn.Module):
+
+    def __init__(self):
+        super(TransUNet, self).__init__()
+
+        # self.linear1 = torch.nn.Linear(100, 200)
+        # self.activation = torch.nn.ReLU()
+        # self.linear2 = torch.nn.Linear(200, 10)
+        # self.softmax = torch.nn.Softmax()
+
+    def forward(self, x):
+        # x = self.linear1(x)
+        # x = self.activation(x)
+        # x = self.linear2(x)
+        # x = self.softmax(x)
+        return x
